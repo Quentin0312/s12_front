aec3a7647f64aba651c4e8b1663449e69f1b28ff
Quentin PAYET <quentinpayet1203@gmail.com>
2023-10-17 18:57:26 +0400

    algo is working, tested with raw inputs

--- src/components/iaGame.tsx
+++ src/components/iaGame.tsx
@@ -9,6 +9,31 @@ import networkWeights from "../ia/networkWeights.json";
 export const bot = Network.fromJSON(networkWeights);
 
 export default function () {
+  // ------------- Preuve que ia_alphabeta_js fonctionne --------------
+  const iaWorker = new Worker("/src/ia_alphabeta_js/minimax.js");
+  iaWorker.addEventListener(
+    "message",
+    function handler(e) {
+      const bestMove = e.data;
+      console.log(bestMove);
+    },
+    false
+  );
+  const workerParams = {
+    matrixBoard: [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0],
+    ],
+    depth: "4", // ! params to modify to change difficulty !
+    maximizingPlayer: false,
+  };
+  iaWorker.postMessage(JSON.stringify(workerParams));
+  // ------------- Preuve que ia_alphabeta_js fonctionne --------------
+
   setPlayerPieceColor(PieceEnum.red);
   return (
     <GameContextRefactored>
