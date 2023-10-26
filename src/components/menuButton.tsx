d68225aac61e0e5b8e2fd72a2d4d6f8f19bbe575
DevIA05 <111973830+DevIA05@users.noreply.github.com>
2023-10-26 16:17:59 +0400

    changement de style

--- src/components/menuButton.tsx
+++ src/components/menuButton.tsx
@@ -5,11 +5,16 @@ type MenuButtonProps = {
 
 export default function (props: MenuButtonProps) {
   return (
-    <div
-      class="mx-5 cursor-pointer select-none"
-      onClick={() => props.onClick()}
-    >
-      <div class="text-center h-fit">{props.title}</div>
-    </div>
+    // <div
+    //   class="mx-5 cursor-pointer select-none"
+    //   onClick={() => props.onClick()}
+    // >
+    //   <div class="text-center h-fit">{props.title}</div>
+    // </div>
+    <button class="btn btn-neutral m-1 w-64"
+            onClick={() => props.onClick()}> 
+          {props.title}
+    </button>
   );
 }
+
