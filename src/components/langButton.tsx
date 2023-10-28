3f6b14910a75c5dbfe62f3fc1c1fc34a0fa41e8f
Quentin PAYET <quentinpayet1203@gmail.com>
2023-10-28 12:11:56 +0400

    swithing lang working

---
+++ src/components/langButton.tsx
@@ -0,0 +1,19 @@
+import { gameLanguage, setGameLanguage } from "../App";
+import { LanguageEnum } from "../languageDict";
+
+function onClick() {
+  setGameLanguage((prev) =>
+    prev == LanguageEnum.fr ? LanguageEnum.eng : LanguageEnum.fr
+  );
+}
+
+export default function () {
+  return (
+    <div
+      class="absolute right-5  top-1/2 ml-2 cursor-pointer select-none"
+      onClick={onClick}
+    >
+      {gameLanguage() == LanguageEnum.fr ? "ENG" : "FR"}
+    </div>
+  );
+}
