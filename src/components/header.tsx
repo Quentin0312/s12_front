4339619cbad836994e25743ecea2709e3a216e14
DevIA05 <dia05@polenumerique.re>
2023-10-29 22:24:14 +0400

    correction: traduction

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,9 @@ import { gameTitle } from "../languageDict";
 import HeaderButton from "./headerButton";
 import LangButton from "./langButton";
 import "./anime-title.css";
+import { createSignal } from "solid-js";
 // TODO: Utiliser le theme tailwind pour les couleurs OU un fichier constant
 export default function () {
-  const title = gameTitle[gameLanguage()]
   return (
     <div class="relative w-full h-36 z-header flex justify-center flex-wrap content-center">
       <HeaderButton />
@@ -15,7 +15,7 @@ export default function () {
                  md:text-[75px]"
       >
         <div class="container-title">
-          {title.split('').map((letter, index) => <span style={`animation-delay: ${index * 0.2}s`}>{letter}</span>)}
+          {gameTitle[gameLanguage()].split('').map((letter, index) => <span style={`animation-delay: ${index * 0.2}s`}>{letter}</span>)}
         </div>
       </h1>
     </div>
