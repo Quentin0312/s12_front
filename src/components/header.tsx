aa32a087a87c650de0699b003671c31ad9381f62
DevIA05 <dia05@polenumerique.re>
2023-10-29 22:09:37 +0400

    animation du titre

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,10 @@ import { gameLanguage } from "../App";
 import { gameTitle } from "../languageDict";
 import HeaderButton from "./headerButton";
 import LangButton from "./langButton";
+import "./anime-title.css";
 // TODO: Utiliser le theme tailwind pour les couleurs OU un fichier constant
 export default function () {
+  const title = gameTitle[gameLanguage()]
   return (
     <div class="relative w-full h-36 z-header flex justify-center flex-wrap content-center">
       <HeaderButton />
@@ -12,7 +14,9 @@ export default function () {
         class="text-center font-extrabold select-none text-red-600 text-[55px] 
                  md:text-[75px]"
       >
-        {gameTitle[gameLanguage()]}
+        <div class="container-title">
+          {title.split('').map((letter, index) => <span style={`animation-delay: ${index * 0.2}s`}>{letter}</span>)}
+        </div>
       </h1>
     </div>
   );
